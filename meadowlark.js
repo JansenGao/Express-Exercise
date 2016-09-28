1e61f7c7090f0274352156f4b6cf1c7cfdea2c81
JansenGao <298066717@qq.com>
2016-09-29 01:05:25 +0800

    Add Express 7.4.6 Partial file

--- meadowlark.js
+++ meadowlark.js
@@ -10,6 +10,34 @@ app.set('view engine', 'handlebars');
 
 var fortune = require('./lib/fortune.js');
 
+function getWeatherData(){
+    return{
+        locations: [
+            {
+                name: "Portland",
+                forcastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: "Bend",
+                forcastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forcastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
 // var fortunes = [
 // "Conquer your fear or they will conquer you.",
 // "Rivers need springs.",
@@ -20,6 +48,14 @@ var fortune = require('./lib/fortune.js');
 
 
 //设置路由
+app.use(function(req,res,next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    //如果有即显消息，把它传到上下文中，然后清除它
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+	next();
+})
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
